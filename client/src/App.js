c0ec6ff4eb8f62bfeb450dcb5ee404ad95d93eeb
Philippe <phildoreau45@gmail.com>
2018-12-02 21:55:07 +0100

    Dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ import Landing from "./components/layout/Landing";
 import Footer from "./components/layout/Footer";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
+import Dashboard from "./components/dashboard/Dashboard";
 
 import "./App.css";
 
@@ -45,6 +46,7 @@ class App extends Component {
             <div className="container">
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
+              <Route exact path="/dashboard" component={Dashboard} />
             </div>
             <Footer />
           </div>
---
+++ client/src/actions/profileActions.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+import { GET_PROFILE, PROFILE_LOADING, GET_ERRORS } from "./types";
+
+export const getCurrentProfile = () => dispatch => {
+  dispatch(setProfileLoading());
+  axios
+    .get("/api/profile")
+    .then(res =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: {}
+      })
+    );
+};
+
+// Profile loading
+export const setProfileLoading = () => {
+  return {
+    type: PROFILE_LOADING
+  };
+};
