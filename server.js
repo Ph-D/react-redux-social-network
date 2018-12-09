6bcd97fe3056711921a966a2c7fa8ca7d2329abd
Philippe <phildoreau45@gmail.com>
2018-12-09 16:24:05 +0100

    removed port client 5000

--- server.js
+++ server.js
@@ -50,4 +50,7 @@ if (process.env.NODE_ENV === "production") {
 // process.env / heroku deployment process
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+//app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () => {
+  console.log("App is running on port " + port);
+});
